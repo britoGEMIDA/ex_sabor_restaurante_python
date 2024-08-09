d52dd5e202596e0276189269fd43d08f2bfbe410
fernando brito <130664535+britoGEMIDA@users.noreply.github.com>
2024-08-09 16:31:52 -0300

    ,

---
+++ app.py
@@ -0,0 +1,27 @@
+# Import da biblioteca os
+import os
+
+# Lista de dicionários representando os restaurantes
+restaurantes = [{'nome': 'Praça','categoria': 'Japonesa', 'ativo': False},
+                {'nome': 'Praça','categoria': 'Japonesa', 'ativo': True},
+                {'nome': 'Praça','categoria': 'Japonesa', 'ativo': False}]
+
+def exibir_nome_do_programa():
+    print("""
+
+𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤
+            """)
+    
+    #Função que exibe o Menu de opções da aplicação
+    def exibir_opções():
+        print('1. Cadastrar resturante')
+        print('2. Listar restaurantes')
+        print('3. Alterar estado do restaurante')
+        print('4. Sair\n')
+
+        #Função que finaliza app
+        def finalizar app():
+        exibir_subtitulo('Finalizando o app\n')
+
+
+        #Função
