151f92b9b0e3c0f66745337bcab793ecb93a536f
fernando brito <130664535+britoGEMIDA@users.noreply.github.com>
2024-08-23 14:15:07 -0300

    .

---
+++ modelos/avaliacao.py
@@ -0,0 +1,12 @@
+class Avaliacao:
+    def __init__(self, cliente, nota):
+        self._cliente = cliente # Nome do clinte que fez a avaliação
+        self._nota = nota # Nota dada pelo cliente
+
+
+    # Método para converter o objeto Avaliacao em um dicionário para salvar em JSON
+    def __dict__(self):
+        return {
+            'cliente': self._cliente,
+            'nota': self._nota
+        }
